1e93664958930f9d129d24764ca6b0d395911b7f
Tomenzo <tomascolkevicius@gmail.com>
2025-09-26 00:38:40 +0300

    Delete input.tsx

--- input.tsx
+++
@@ -1,5 +0,0 @@
-
-import * as React from 'react'
-export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
-  return <input {...props} className={`h-10 rounded-md border bg-transparent px-3 ${props.className || ''}`} />
-}
