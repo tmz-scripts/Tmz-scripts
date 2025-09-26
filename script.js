59fd0be784cc920144fff20ee2b006e51de7d3b0
Tomenzo <tomascolkevicius@gmail.com>
2025-09-26 19:52:11 +0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('y').textContent=new Date().getFullYear();
-document.getElementById('enterBtn').addEventListener('click',()=>document.getElementById('intro').classList.add('hidden'));
-document.getElementById('openMenu').addEventListener('click',()=>document.getElementById('sidebar').classList.add('open'));
-document.getElementById('closeMenu').addEventListener('click',()=>document.getElementById('sidebar').classList.remove('open'));
